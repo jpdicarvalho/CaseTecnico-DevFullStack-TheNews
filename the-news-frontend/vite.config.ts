65b441e13a412af48c6672ba6d31802f39c25983
jpdicarvalho <joaopedrobraga.07@gmail.com>
2025-02-24 01:19:07 -0300

    Update vite.config.ts

--- the-news-frontend/vite.config.ts
+++ the-news-frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/the-news-frontend/', // Defina o nome correto do repositório
+  base: '/TheNews-FullStack-Project/', // Nome correto do repositório
 });
